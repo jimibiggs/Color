fcfa08e02ec7ad03adcd3058f8f0c3cf8bbba153
Dev Revonema <devrevonema@mac-mini-de-dev.local>
2016-04-24 15:03:46 +0200

    ddd

--- ColorGame/ViewController.swift
+++ ColorGame/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //commebt
+        //comment ou comment
         
         // Do any additional setup after loading the view, typically from a nib.
     }
