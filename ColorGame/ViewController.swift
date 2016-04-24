6ce19809fd1a9599735dfdc8c7377582495fc819
Dev Revonema <devrevonema@mac-mini-de-dev.local>
2016-04-24 14:46:01 +0200

    Initial Commit

---
+++ ColorGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorGame
+//
+//  Created by Dev Revonema on 24/04/2016.
+//  Copyright © 2016 Dev Revonema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
